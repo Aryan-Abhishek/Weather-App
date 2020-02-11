ebed8d127c568d6d39354cbbfb707baa079c22ae
Aryan-Abhishek <arya2001abhishek@gmail.com>
2020-02-11 10:30:09 +0530

    added modules

--- utils/forecast.js
+++ utils/forecast.js
@@ -4,13 +4,13 @@ const forecast = (latitude, longitude, callback) => {
 
     const url = 'https://api.darksky.net/forecast/38ca92b4206afef0c7003e2df3676fe3/' + latitude + ',' + longitude
 
-    request({url: url, json: true}, (error, response) => {
+    request({url, json: true}, (error, { body }) => {
         if (error) {
             callback('Unable to connect to Weather services!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location! Please try again..', undefined)
         } else {
-            callback(undefined, 'It\'s ' + response.body.daily.data[0].summary + 'currently ' + response.body.currently.temperature + ' degrees out. There is ' + response.body.currently.precipProbability*100 + '% chance of rain.')
+            callback(undefined, 'It\'s ' + body.daily.data[0].summary + 'currently ' + body.currently.temperature + ' degrees out. There is ' + body.currently.precipProbability*100 + '% chance of rain.')
         }
     })
 }
--- utils/geocode.js
+++ utils/geocode.js
@@ -3,17 +3,17 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYXJ5YS1hYmhpc2hlayIsImEiOiJjazZmb2NjMzYxd2lhM2ttZzVvb3k1cHFsIn0.AyZmSOcNANp5ZIwe1-aW3g&limit=1'
 
-    request({url: url, json:true}, (error, response) => {
+    request({url, json: true}, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather services!', undefined)
-        } else if (response.body.features.length === 0) {
+        } else if (body.features.length === 0) {
             callback('Unable to find match for given place! Try another search..', undefined)
         } else {
             callback(undefined, {
                 //since we have to return three values hence return an object of the same
-                location: response.body.features[0].place_name,
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
             })
         }
     })
